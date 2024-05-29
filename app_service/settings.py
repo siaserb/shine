57567ea79d00de8541b87a84e5b40da21cfe1558
siaserb <assiya.serb@gmail.com>
2024-05-29 17:00:26 +0300

    remove insecure django secret key for deployment and change debug mode settings

--- app_service/settings.py
+++ app_service/settings.py
@@ -22,13 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-$-o4@3vj9wus1^we8+t$9h5@st4sco40#l97t142wd1-!^c@i="
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
