e63640cd551f1289b98055865243078fa0ce30bf
siaserb <assiya.serb@gmail.com>
2024-05-29 22:07:12 +0300

    ALLOWED_HOSTS fixed

--- app_service/settings.py
+++ app_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "shine-wy53.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "shine-wy53.onrender.com"]
 
 # Application definition
 
@@ -144,4 +144,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
